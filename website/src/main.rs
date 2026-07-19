31d8251199d161eaf7a0538d225e4c157b9c7fb7
kitswas <90329875+kitswas@users.noreply.github.com>
2026-07-19 19:56:34 +0530

    feat: add website showcase GH Pages deployment

---
+++ website/src/main.rs
@@ -0,0 +1,41 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod app;
+use app::ShowcaseApp;
+use wasm_bindgen::JsCast;
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+	eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+	let web_options = eframe::WebOptions::default();
+	wasm_bindgen_futures::spawn_local(async {
+		let document = web_sys::window().unwrap().document().unwrap();
+		let canvas = document
+			.get_element_by_id("the_canvas_id")
+			.unwrap()
+			.dyn_into::<web_sys::HtmlCanvasElement>()
+			.unwrap();
+
+		let start_result = eframe::WebRunner::new()
+			.start(
+				canvas,
+				web_options,
+				Box::new(|cc| Ok(Box::new(ShowcaseApp::new(cc)))),
+			)
+			.await;
+		if let Err(e) = start_result {
+			log::error!("Failed to start eframe: {e:?}");
+		}
+	});
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+fn main() {
+	let options = eframe::NativeOptions::default();
+	eframe::run_native(
+		"egui-elegant showcase",
+		options,
+		Box::new(|cc| Ok(Box::new(ShowcaseApp::new(cc)))),
+	)
+	.unwrap();
+}
